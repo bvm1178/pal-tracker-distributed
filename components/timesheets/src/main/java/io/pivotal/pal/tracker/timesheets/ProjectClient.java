a862b02563e147998d3df8c20bfefb697d879b7d
bvm1178 <bvm1178@gmail.com>
2017-07-25 02:29:27 +0300

    circuit-breakers lab changes

--- components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
+++ components/timesheets/src/main/java/io/pivotal/pal/tracker/timesheets/ProjectClient.java
@@ -1,9 +1,20 @@
 package io.pivotal.pal.tracker.timesheets;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestOperations;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 
 public class ProjectClient {
 
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
+	
     private final RestOperations restOperations;
     private final String endpoint;
 
@@ -15,4 +26,10 @@ public class ProjectClient {
     public ProjectInfo getProject(long projectId) {
         return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
     }
+    
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
+	public ProjectInfo getProjectFromCache(long projectId) {
+		logger.info("Getting project with id {} from cache", projectId);
+		return projectsCache.get(projectId);
+	}
 }
