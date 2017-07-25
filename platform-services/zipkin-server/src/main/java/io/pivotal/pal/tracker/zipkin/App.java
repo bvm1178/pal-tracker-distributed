96026ffc560f0343beb877d8f00dfbf1215a6af8
bvm1178 <bvm1178@gmail.com>
2017-07-26 00:35:39 +0300

    zipkin lab changes

---
+++ platform-services/zipkin-server/src/main/java/io/pivotal/pal/tracker/zipkin/App.java
@@ -0,0 +1,17 @@
+package io.pivotal.pal.tracker.zipkin;
+
+import java.util.TimeZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class App {
+	
+	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+		SpringApplication.run(App.class, args);
+	}
+}
